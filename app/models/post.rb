516ef9cb349973c6b67bb21c6d5b734cd3c34a4b
Jay Shenk <jay.shenk@gmail.com>
2015-08-25 20:45:44 -0400

    Add Post model

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :url, length: { maximum: 255 }
+  validates :user_id, presence: true
+
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
             uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+
+  has_many :posts
 end
